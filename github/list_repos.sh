f32418076d8ad8f3f39c1d8d8e67fe08450e967a
Chris Patterson <pattersonc@ainfosec.com>
2017-03-24 11:14:32 -0400

    github/list_repos.sh: support tokenless request
    Signed-off-by: Chris Patterson <pattersonc@ainfosec.com>

--- github/list_repos.sh
+++ github/list_repos.sh
@@ -2,9 +2,14 @@
 
 # Generate a token there: https://github.com/settings/tokens
 TOKEN=$1
+TOKEN_ARGS=""
+
+if [[ -n "${TOKEN}" ]]; then
+    TOKEN_ARGS="-H \"Authorization: token $TOKEN\""
+fi
 
 # Get the list of OpenXT repos
-repos=`curl -H "Authorization: token $TOKEN" -s "https://api.github.com/users/openxt/repos?per_page=100" | jq '.[].name' | cut -d '"' -f 2`
+repos=`curl ${TOKEN_ARGS} -s "https://api.github.com/users/openxt/repos?per_page=100" | jq '.[].name' | cut -d '"' -f 2`
 
 for i in $repos;
 do
